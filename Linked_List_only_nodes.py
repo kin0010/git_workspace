817f8f67789de58bddec548bb43d56c31ad66e71
87479106+kin0010@users.noreply.github.com <psbhat007@gmail.com>
2021-10-06 11:14:40 +0530

    learned how to insert nodes and print it

--- Linked_List_only_nodes.py
+++ Linked_List_only_nodes.py
@@ -1,13 +1,14 @@
 class LinkedListNode:
-    def __init__(self, value, nextNode = None):
+    def __init__(self, value, nextNode=None):
         self.value = value
         self.nextNode = nextNode
 
+
 # 3 -> 7 -> 10
 
-node1 = LinkedListNode('3') # '3'
-node2 = LinkedListNode('7') # '7'
-node3 = LinkedListNode('10') # '10'
+node1 = LinkedListNode('3')  # '3'
+node2 = LinkedListNode('7')  # '7'
+node3 = LinkedListNode('10')  # '10'
 
 node1.nextNode = node2
 node2.nextNode = node3
--- linked_list_full_class.py
+++ linked_list_full_class.py
@@ -12,3 +12,25 @@ class LinkedList:
         if self.head is None:
             self.head = node
             return
+        currentNode = self.head
+        while True:
+            if currentNode.nextNode is None:
+                currentNode.nextNode = node
+                break
+            currentNode = currentNode.nextNode
+
+    def printLinkedList(self):
+        currentNode = self.head
+        while currentNode is not None:
+            print(currentNode.value + '->')
+            currentNode = currentNode.nextNode
+        else:
+            print(None)
+
+
+ll = LinkedList()
+ll.printLinkedList()
+ll.insert('3')
+ll.printLinkedList()
+ll.insert('44')
+ll.printLinkedList()
