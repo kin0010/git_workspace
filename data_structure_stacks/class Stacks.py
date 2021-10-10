4773270d61208f888e3d15deb60432c52d9813cb
87479106+kin0010@users.noreply.github.com <psbhat007@gmail.com>
2021-10-10 22:23:43 +0530

    coded Towers of Hanoi game with stacks

--- data_structure_stacks/class Stacks.py
+++ data_structure_stacks/class Stacks.py
@@ -14,19 +14,42 @@ class Node:
 
 
 class Stack:
-    def __init__(self):
+    def __init__(self, limit=1000):
         self.top_item = None
+        self.size = 0
+        self.limit = limit
 
-    # Define your push() and pop() methods below:
     def push(self, value):
-        item = Node(value)
-        item.set_next_node(self.top_item)
-        self.top_item = item
+        if self.has_space():
+            item = Node(value)
+            item.set_next_node(self.top_item)
+            self.top_item = item
+            # Increment stack size by 1 here:
+            self.size += 1
+        else:
+            print('All out of space!')
 
     def pop(self):
-        item_to_remove = self.top_item
-        self.top_item = item_to_remove.get_next_node()
-        return item_to_remove.value
+        if self.size > 0:
+            item_to_remove = self.top_item
+            self.top_item = item_to_remove.get_next_node()
+            self.size -= 1
+            return item_to_remove.get_value()
+        else:
+            print("This stack is totally empty.")
 
     def peek(self):
-        return self.top_item.get_value()
+        if not self.is_empty():
+            return self.top_item.get_value()
+        else:
+            print("Nothing to see here!")
+
+    # Define has_space() and is_empty() below:
+    def has_space(self):
+        return self.limit > self.size
+
+    def is_empty(self):
+        return self.size == 0
+
+babies = Stack()
+babies.peek()
